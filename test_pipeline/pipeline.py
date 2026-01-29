0e8b2a17b8ddac8268acd273e616c56b3ba78540
Ben Wengreen <benson.wengreen@gmail.com>
2026-01-28 16:37:22 -0800

    Add Prompt class and framework for Pipeline class

---
+++ test_pipeline/pipeline.py
@@ -0,0 +1,24 @@
+class Pipeline:
+    def __init__(self):
+        pass
+
+    def download_textbook(url):
+        pass
+
+    def upload_file(file_path):
+        pass
+
+    def identify_knowledge(uploaded_file):
+        pass
+    
+    def generate_question(content, level, difficulty):
+        pass
+
+    def generate_rubric(problem_statement, level):
+        pass
+
+    def generate_feedback(problem, rubric, student_answer):
+        pass
+
+    def generate_grading_report(student_id, assignment_id, problem_id, difficulty, problem_statement, allowed_concepts, rubric):
+        pass
---
+++ test_pipeline/utils/prompt.py
@@ -0,0 +1,47 @@
+import re
+
+class Prompt:
+    def get_parameter_names(self) -> str:
+        parameter_names = re.findall(r'\{\{(.*?)\}\}', self.raw_prompt)
+        valid_parameter_names = [name for name in parameter_names if not bool(re.search(r'\s', name))]
+
+        assert len(valid_parameter_names) == len(parameter_names) and len(valid_parameter_names) == len(set(valid_parameter_names)), "Parameter names must not contain whitespace."
+
+        return valid_parameter_names
+
+    def __init__(self, raw_prompt: str):
+        self.raw_prompt = raw_prompt
+        self.parameter_names = self.get_parameter_names()
+
+    def arguments_to_content(self, **kwargs):
+        pattern = r"\{\{.*?\}\}"
+        split_prompt = re.split(pattern, self.raw_prompt)
+
+        filled_prompt = split_prompt.copy()
+
+        for i in reversed(range(len(split_prompt) - 1)):
+            filled_prompt.insert(i + 1, kwargs[self.parameter_names[i]])
+
+        if len(filled_prompt) == 1:
+            return filled_prompt
+        
+        index = 1
+        while index < len(filled_prompt):
+            if filled_prompt[index] == "":
+                filled_prompt.pop(index)
+            elif type(filled_prompt[index]) is str and type(filled_prompt[index - 1]) is str:
+                filled_prompt[index - 1] += filled_prompt[index]
+                filled_prompt.pop(index)
+            else:
+                index += 1
+
+        return filled_prompt
+
+if __name__ == "__main__":
+    prompt = Prompt("Hello, {{NAME}}! Welcome to {{PLACE}}. {{NON_STRING}}")
+    arguments = {"NAME": "Alice", "PLACE": "Wonderland", "NON_STRING": 123}
+    filled = prompt.arguments_to_content(**arguments)
+    print(prompt.raw_prompt)
+    print(arguments)
+    print(filled)
+    assert filled == ["Hello, Alice! Welcome to Wonderland. ", 123]
